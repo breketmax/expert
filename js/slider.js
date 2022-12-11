3e53bbba564a54d93b2522cd1ba38bb93b46ac2f
breketmax <maxim_228228228@mail.ru>
2022-12-11 23:41:28 +0700

    Rework slider

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,6 @@
 
 const wrapper = document.querySelector(".slider-wrapper")
-const slider = document.querySelector("#partners");
+const slider = document.querySelector(".slider");
 const nextBtn = document.querySelector(".slider-next");
 const prevBtn = document.querySelector(".slider-prev");
 
@@ -10,79 +10,139 @@ let move = 0;
 let counter=0;
 let transalteCount =0;
 let wrapperFullWidth = 0;
+let previousLeft =0;
+let newLeft=0
 
-const slides= document.querySelectorAll(".slide")
-const containerWidth =  document.querySelector(".container").clientWidth;
 
-slides.forEach(slide =>{
-  wrapperFullWidth+=slide.offsetWidth
-})
 
-wrapperFullWidth= wrapperFullWidth - containerWidth + 24 + 9;
+const imgs = wrapper.querySelectorAll(".slider img");
+let unloadedImgs = imgs.length;
 
-const imgs = wrapper.querySelectorAll("img");
+const checkLoad = () => {
+  --unloadedImgs;
+  console.log(unloadedImgs);
+  if(!unloadedImgs){
+    const slides= document.querySelectorAll(".slide");   
+    const containerWidth =  document.querySelector(".sliderContainer").clientWidth;
+    slides.forEach(slide =>{
+      wrapperFullWidth+=slide.offsetWidth;
+      console.log("before sub",wrapperFullWidth );
+    })
+    wrapperFullWidth= wrapperFullWidth - containerWidth + 24;
+    addSliderHandlers();
+    console.log("load all img");
+  }  
+}
+
+
 imgs.forEach(i => {
+  i.onload = checkLoad;
   i.addEventListener("dragstart", (e)=>{
-    e.preventDefault()
+    e.preventDefault();
   }) 
 })
 wrapper.addEventListener("dragstart", (e)=>{
   e.preventDefault()
 }) 
-wrapper.addEventListener("mousedown",(e)=>{
-  wrapper.style.transition = "none"
-   startPos = e.clientX;
-   wrapper.addEventListener("mousemove",function handler(evt){
-   
-    let dif = startPos - evt.clientX;
-    if(dif>0){
-      counter-=9
-    }
-    else{
-      counter+=9
-    }
-    
-    if(counter <= -wrapperFullWidth){
-      nextBtn.classList.add("inactive");
-      counter = -wrapperFullWidth;
-      wrapper.addEventListener("mouseup",()=>{
-        wrapper.removeEventListener("mousemove",handler)
-      })
-      return;
-    }
-    if(counter >=9){
-      prevBtn.classList.add("inactive");
-      counter =0;
-      wrapper.addEventListener("mouseup",()=>{
-        wrapper.removeEventListener("mousemove",handler)
-      })
-      return;
-    }
 
-    moveWrapper(counter)
-    wrapper.addEventListener("mouseup",()=>{
-      wrapper.removeEventListener("mousemove",handler)
-    })
-  })
-})
+
+const addSliderHandlers =() =>{
+  console.log(wrapperFullWidth);
+  wrapper.onpointerdown = function(event) {
+
+    let downCoords = event.clientX;
+    wrapper.setPointerCapture(event.pointerId);
+    wrapper.onpointermove = function(event) {
+
+      if(downCoords>event.clientX){
+        newLeft = previousLeft +  event.clientX - slider.getBoundingClientRect().left;
+      moveWrapper(-newLeft);
+      }
+      else{
+        newLeft = previousLeft -  event.clientX - slider.getBoundingClientRect().left;
+        moveWrapper(-newLeft);
+      }
+
+    };
+
+    wrapper.onpointerup = function(event) {
+      wrapper.onpointermove = null;
+      wrapper.onpointerup = null;
+      previousLeft = newLeft;
+
+    };
+  };
+}
+
+
+// wrapper.addEventListener("pointerdown",(e)=>{
+//   wrapper.style.transition = "none"
+//    startPos = e.clientX;
+//    wrapper.addEventListener("pointermove",function handler(evt){
+
+//     let dif = startPos - evt.clientX;
+//     if(dif>0){
+//       counter-=9
+//     }
+//     else{
+//       counter+=9
+//     }
+    
+//     if(counter <= -wrapperFullWidth){
+//       nextBtn.classList.add("inactive");
+//       counter = -wrapperFullWidth;
+//       wrapper.addEventListener("pointerup",()=>{
+//         wrapper.removeEventListener("pointermove",handler);
+//       })
+//       return;
+//     }
+//     if(counter >=9){
+//       prevBtn.classList.add("inactive");
+//       counter =0;
+//       wrapper.addEventListener("pointerup",()=>{
+//         wrapper.removeEventListener("pointermove",handler)
+//       })
+//       return;
+//     }
+
+//     moveWrapper(counter);
+//     console.log(counter);
+//     wrapper.addEventListener("pointerup",()=>{
+
+//       wrapper.removeEventListener("pointermove",handler)
+//     })
+//   })
+// })
+
+
+
 
 const moveWrapper = (length)=>{
-  wrapper.style.transform=`translate(${length}px,0px)`;
-  if(length !=0 && length !=-wrapperFullWidth){
-    prevBtn.classList.remove("inactive");
-    nextBtn.classList.remove("inactive");
-  }
 
+  if(length >=0 ){
+    wrapper.style.transform=`translate(${0}px,0px)`; 
+    prevBtn.classList.add("inactive");
+    previousLeft=0;
+    return; 
+  }
+  else if(length <= -wrapperFullWidth){
+    wrapper.style.transform=`translate(${-wrapperFullWidth}px,0px)`;
+    nextBtn.classList.add("inactive");
+    previousLeft=wrapperFullWidth;
+    return;
+  }
+  wrapper.style.transform=`translate(${length}px,0px)`;  
+  prevBtn.classList.remove("inactive");
+  nextBtn.classList.remove("inactive");
 }
 
 
 nextBtn.addEventListener("click",()=>{
-
-  wrapper.style.transition = "all 0.5s";
-  counter = counter-300;
+  wrapper.style.transition = "all 0.5s ease";
+  counter = -previousLeft-300;
+  previousLeft+=300;
   if(counter < -wrapperFullWidth){
     counter = -wrapperFullWidth;
-    nextBtn.classList.add("inactive");
   }
   moveWrapper(counter);
 
@@ -90,12 +150,11 @@ nextBtn.addEventListener("click",()=>{
 
 
 prevBtn.addEventListener("click",()=>{
-  wrapper.style.transition = "all 0.5s";
-  counter=counter+300;
-  if(counter >=0){
+  wrapper.style.transition = "all 0.5s ease";
+  counter=previousLeft-300;
+  previousLeft-=300;
+  if(counter <= 0){
     counter =0;
-    prevBtn.classList.add("inactive");
   }
-  moveWrapper(counter);
-
+  moveWrapper(-counter);
 })
--- mail.php
+++ mail.php
@@ -1,33 +1,62 @@
-<meta http-equiv='refresh' content='3'; url='http://kemosago.ru/'>
+<meta http-equiv='refresh' content='20'; url='http://kemosago.ru'>
 <meta charset="UTF-8" />
 <?php
-// if (isset($_POST['name']) && $_POST['name'] != "" )//если существует атрибут NAME и он не пустой то создаем переменную для отправки сообщения
-// 	$name = $_POST['name'];
-// else die ("Не заполнено поле \"Имя\"");//если же атрибут пустой или не существует то завершаем выполнение скрипта и выдаем ошибку пользователю.
-// if ((isset($_POST['email']) && $_POST['email'] != "") || (isset($_POST['subjects']) && $_POST['subjects'] != "")){ //тут все точно так же как и в предыдущем случае
-// 	$email = $_POST['email'];
-// 	$sub = $_POST['subjects'];
-// }
-// else die ("Не заполнено поле \"Электронная Почта или Номер Телефона\"");
-// if (isset($_POST['message']) && $_POST['message'] != "") 
-// 	$body = $_POST['message'];
-// else die ("Не заполнено поле \"Текст Сообщение\"");
- 
 $name = $_POST['name'];
 $email = $_POST['e-mail'];
 $phone = $_POST['phone'];
 $text = $_POST['message'];
 
+$home= "http://kemosago.ru/";
 
-$address = "maxim_228228228@mail.ru";//адрес куда будет отсылаться сообщение для администратора
-$mes  = "Имя: $name \n";	//в этих строчках мы заполняем текст сообщения. С помощью оператора .= мы просто дополняем текст в переменную
-$mes .= "E-mail: $email \n";
-$mes .= "Телефон: $phone \n";
-$mes .= "Текст: $text"; 
-$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");//собственно сам вызов функции отправки сообщения на сервере
-if ($send) //проверяем, отправилось ли сообщение
-	echo "Сообщение отправлено успешно! Перейти на <a href='http://kemosago.ru'>kemosago.ru</a>, если вас не перенаправило вручную.";
-else 
-	echo "Ошибка, сообщение не отправлено! Возможно, проблемы на сервере";
-	 
-?>
+$sub = "Вопрос от клиента с сайта kemosago.ru";
+
+$to = "maxim_228228228@mail.ru";//адрес куда будет отсылаться сообщение для администратора
+$mes = "
+				<html>
+					<head>
+						<title>Вопрос от клиента с сайта kemosago.ru</title> 
+					</head>
+					<body style=' font-family:'Tahoma',  sans-serif;'>	
+						<table style='max-width: 600px; border-spacing: 10px 10px; margin:0 auto;'>
+							<tr style='border:1px solid transparent'>
+								<td>
+									<div style='background-color:#3172B6; color:#ffffff; text-align:center; padding:25px 60px; ' >
+										<h1 style='font-size: 50px; margin:0 ;line-height:55px'>Вопрос от клиента</h1>
+									</div>
+								</td>
+							</tr>
+							<tr style='border:1px solid transparent;'>
+								<td>
+									<div style='background-color:#ffffff;color:#2D3038;padding: 20px 25px 70px; width:600px'>
+										<img src='http://kemosago.ru/img/logo2.png' style='width: 30%; margin:0 auto; display: block;'>
+										<h1 style='font-size: 35px; text-align: center; margin: 0; padding: 0; font-weight: bold;margin-top:10px;margin-bottom:20px;'>ОСАГО <br/> <span style='font-size: 30px; font-weight: 400;'>Эксперт</span></h1>							
+										<h2 style='font-size: 30px; font-weight: bold; margin: 0; padding: 0; ;margin-top: 30px;margin-bottom:30px;'>$name</h2>
+										<h3 style='font-size: 20px; margin: 0; padding: 0;margin-top: 5px;' >Номер телефона: <a style='text-decoration: none; color:#3172B6' href='tel:+$phone'>$phone</a></h3>
+										<h3 style='font-size: 20px; margin: 0; padding: 0;margin-top: 5px;'>Электронная почта: <a style='text-decoration: none; color:#3172B6' href='mailto:+$email'>$email</a></h3>
+										<h3 style='font-size: 20px; margin: 0; padding: 0;margin-top: 5px;'>Текст вопроса: <br/><span style='font-weight:400'> $text</span></h3>
+									</div>  
+								</td>  
+							</tr>
+						</table>
+					</body>
+				</html>
+			";
+			
+$headers = "MIME-Version: 1.0" . "\r\n"; 
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+if(!empty($_POST['name'])){
+    $send = mail($to,$sub,$mes,$headers);   
+    echo "<div style='text-align:center; background-color:#fff; font-family:Tahoma,  sans-serif; color:#2D3038; height:100vh; display:flex; flex-direction:column;justify-content: center;align-items:center'>
+        <img src='http://kemosago.ru/img/check.svg' style='width:100px;'>
+        <h1 style='color:#2D3038; width:100%; text-align:center;'>Ваш вопрос успешно отправлен!</h1>
+        
+    </div>";
+    ?>
+    <script type="text/javascript">
+       setTimeout(()=>{
+            window.location.href = "http://kemosago.ru#feedback"     
+       },3000)
+    </script>
+    
+    <?php
+}
